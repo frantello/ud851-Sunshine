f527f968c1955a97e08f89a33e86c126337a2d23
Fran Tello <jfrantello@gmail.com>
2017-03-28 09:22:19 +0200

    Synchronizing The Weather

--- S10.01-Exercise-SynchronizingTheWeather/app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ S10.01-Exercise-SynchronizingTheWeather/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -34,7 +34,7 @@ import android.widget.ProgressBar;
 
 import com.example.android.sunshine.data.SunshinePreferences;
 import com.example.android.sunshine.data.WeatherContract;
-import com.example.android.sunshine.utilities.FakeDataUtils;
+import com.example.android.sunshine.sync.SunshineSyncUtils;
 
 public class MainActivity extends AppCompatActivity implements
         LoaderManager.LoaderCallbacks<Cursor>,
@@ -86,9 +86,7 @@ public class MainActivity extends AppCompatActivity implements
         setContentView(R.layout.activity_forecast);
         getSupportActionBar().setElevation(0f);
 
-        // TODO (12) Remove the fake data creation since we can now sync with live data
-        FakeDataUtils.insertFakeData(this);
-
+        // COMPLETED (12) Remove the fake data creation since we can now sync with live data
         /*
          * Using findViewById, we get a reference to our RecyclerView from xml. This allows us to
          * do things like set the adapter of the RecyclerView and toggle the visibility.
@@ -155,8 +153,8 @@ public class MainActivity extends AppCompatActivity implements
          */
         getSupportLoaderManager().initLoader(ID_FORECAST_LOADER, null, this);
 
-        //  TODO (13) Call SunshineSyncUtils's startImmediateSync method
-
+        //  COMPLETED (13) Call SunshineSyncUtils's startImmediateSync method
+        SunshineSyncUtils.startImmediateSync(this);
     }
 
     /**
--- S10.01-Exercise-SynchronizingTheWeather/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncTask.java
+++ S10.01-Exercise-SynchronizingTheWeather/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncTask.java
@@ -1,4 +1,35 @@
-//  TODO (1) Create a class called SunshineSyncTask
-//  TODO (2) Within SunshineSyncTask, create a synchronized public static void method called syncWeather
-//      TODO (3) Within syncWeather, fetch new weather data
-//      TODO (4) If we have valid results, delete the old data and insert the new
+package com.example.android.sunshine.sync;
+
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.Context;
+
+import com.example.android.sunshine.data.WeatherContract;
+import com.example.android.sunshine.utilities.NetworkUtils;
+import com.example.android.sunshine.utilities.OpenWeatherJsonUtils;
+
+import org.json.JSONException;
+
+import java.io.IOException;
+
+//  COMPLETED (1) Create a class called SunshineSyncTask
+public class SunshineSyncTask {
+
+//  COMPLETED (2) Within SunshineSyncTask, create a synchronized public static void method called syncWeather
+    public static synchronized void syncWeather(Context context) {
+        try {
+//      COMPLETED (3) Within syncWeather, fetch new weather data
+            String raw = NetworkUtils.getResponseFromHttpUrl(NetworkUtils.getUrl(context));
+            ContentValues[] values = OpenWeatherJsonUtils
+                    .getWeatherContentValuesFromJson(context, raw);
+//      COMPLETED (4) If we have valid results, delete the old data and insert the new
+            if (values != null) {
+                ContentResolver resolver = context.getContentResolver();
+                resolver.delete(WeatherContract.WeatherEntry.CONTENT_URI, null, null);
+                resolver.bulkInsert(WeatherContract.WeatherEntry.CONTENT_URI, values);
+            }
+        } catch (IOException | JSONException e) {
+            e.printStackTrace();
+        }
+    }
+}
